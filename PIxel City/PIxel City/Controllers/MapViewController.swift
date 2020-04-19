bae8b46177688e30cc5bf6783d811101b2bad5df
Unknown <chriswm@me.com>
2020-04-19 12:55:43 +0100

    Lessons 1, 2 and 3 - Setup, pods etc

---
+++ PIxel City/PIxel City/Controllers/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PIxel City
+//
+//  Created by Chris Mercer on 19/04/2020.
+//  Copyright © 2020 Chris Mercer. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PIxel City/Pods/Target Support Files/Pods-PIxel City/Pods-PIxel City-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PIxel_City : NSObject
+@end
+@implementation PodsDummy_Pods_PIxel_City
+@end
